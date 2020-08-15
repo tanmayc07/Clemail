c15b68eba731862cac2dea3848250e1d9353173f
tanmayc07 <tanmayc32@gmail.com>
2020-08-15 18:33:53 +0530

    Add smtplib for sending email

--- clemail.py
+++ clemail.py
@@ -1,11 +1,29 @@
 from pyfiglet import Figlet
 import click
+import smtplib
+import ssl
+
+sender_email = "tracer00122132@gmail.com"
+receiver_email = "sahilambre06@gmail.com"
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+port = 465  # For SSL
 
 header = Figlet(font="slant")
 print(header.renderText("CLEMAIL"))
 
 
 @click.command()
-def cli():
+def start():
     """Test script"""
-    click.echo("Hello World")
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("tracer00122132@gmail.com", "tracer00!")
+        server.sendmail(sender_email, receiver_email, message)
+
+    click.echo("Message sent!")
